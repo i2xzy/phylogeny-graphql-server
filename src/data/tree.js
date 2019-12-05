254ccb6f96eca9d8b832dce0716203142e1a49af
Isaac Raskin <yitzchokr@live.com>
2019-12-05 18:19:06 +0000

    move resolvers to api

--- src/data/tree.js
+++ src/data/tree.js
@@ -6,14 +6,38 @@ class TreeAPI extends RESTDataSource {
     this.baseURL = 'https://tree.phylogenyexplorerproject.com/clades/';
   }
 
-  async getTree(id, depth) {
-    return this.get(
+  async getTree({ id, depth }) {
+    const response = await this.get(
       `tree/${id ? `${id}` : ''}/${depth ? `depth/${depth}` : ''}`
     );
+    return this.treeReducer(response);
   }
 
-  async getClade(id) {
-    return this.get(`${id}`);
+  async getClade({ id }) {
+    const response = await this.get(id);
+    return this.cladeReducer(response);
+  }
+
+  treeReducer(tree) {
+    return {
+      ...tree,
+      root: this.cladeReducer(tree.root)
+    };
+  }
+
+  cladeReducer(clade) {
+    return {
+      ...clade,
+      id: clade._id,
+      parent: this.parentReducer(clade.parent),
+      children:
+        clade.children && clade.children.map(child => this.cladeReducer(child))
+    };
+  }
+
+  parentReducer(parent) {
+    if (!parent) return null;
+    return parent._id ? this.cladeReducer(parent) : { id: parent };
   }
 }
 
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,11 +1,7 @@
 export default {
   Query: {
     tree: (_, { id, depth }, { dataSources }) =>
-      dataSources.treeAPI.getTree(id, depth),
-    clade: (_, { id }, { dataSources }) => dataSources.treeAPI.getClade(id)
-  },
-  Clade: {
-    id: clade => clade._id,
-    parentId: clade => clade.parent
+      dataSources.treeAPI.getTree({ id, depth }),
+    clade: (_, { id }, { dataSources }) => dataSources.treeAPI.getClade({ id })
   }
 };
